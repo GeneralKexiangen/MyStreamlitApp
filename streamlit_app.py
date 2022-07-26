47d6d87a276e934d6e3093edf42d982b3766da87
zhiyue <kexiangen@outlook.com>
2022-07-26 14:19:51 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,18 @@ footer {visibility: hidden;}
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.title('WOW!HKK')
 
-mood = st.sidebar.text_input('Write down your mood at right now please.')
+mood = st.sidebar.text_input('Write down your mood at right now please.', 'sunshine')
 take = st.sidebar.checkbox('Take a picture to upload')
 img_file_buffer = None
 if take:
     img_file_buffer = st.sidebar.camera_input("Take a picture")
-if mood and img_file_buffer:
+if mood:
     st.balloons()
-    img = Image.open(img_file_buffer)
-    st.image(img)
-    # st.image('resource/pp.jpg')
+    if img_file_buffer:
+        img = Image.open(img_file_buffer).resize((4784, 3376))
+        img.save('resource/img.jpg')
+        # st.image(img)
+        st.image('resource/img.jpg')
+    else:
+        st.image('resource/pp.jpg')
     st.success(mood)
-
