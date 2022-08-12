958561ca56b56255b6b50d36f2e410695bde48d3
zhiyue <kexiangen@outlook.com>
2022-08-12 15:07:26 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ footer {visibility: hidden;}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.title('WOW!HKK')
+# st.title("WOW!HKK 🎉️")
+st.markdown("# WOW!HKK 🎉️")
 
 mood = st.sidebar.text_input('Write down your mood at right now please.', 'sunshine')
 take = st.sidebar.checkbox('Take a picture to upload')
@@ -23,11 +24,16 @@ if take:
     img_file_buffer = st.sidebar.camera_input("Take a picture")
 if mood:
     st.balloons()
-    if img_file_buffer:
-        img = Image.open(img_file_buffer).resize((4784, 3376))
-        img.save('resource/img.jpg')
-        # st.image(img)
-        st.image('resource/img.jpg')
-    else:
-        st.image('resource/pp.jpg')
-    st.success(mood)
+    c1, c2 = st.columns(2)
+    with c1:
+        st.write("Your Picture")
+        if img_file_buffer:
+            img = Image.open(img_file_buffer).resize((800, 600))
+            img.save('resource/img.jpg')
+            # st.image(img)
+            st.image('resource/img.jpg')
+        else:
+            st.image('resource/pp.jpg')
+    with c2:
+        st.write("Your Text")
+        st.success(mood)
